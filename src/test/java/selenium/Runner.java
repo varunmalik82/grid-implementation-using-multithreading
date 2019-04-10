cade880d44bde4c03ca6de0df03d39cd90fe7ca2
varunmalik82 <varun.malik@milwaukeetool.com>
2019-04-10 15:42:34 +0530

    updated tests

--- src/test/java/selenium/Runner.java
+++ src/test/java/selenium/Runner.java
@@ -1,5 +1,9 @@
 package selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class Runner{
 
     static Thread chromeThread = new Thread(new ChromeTest(){
@@ -20,5 +24,61 @@ public class Runner{
         chromeThread.start();
         safariThread.start();
     }
+
+}
+
+class SafariTest implements Runnable {
+
+    WebDriver driver;
+
+    public void run(){
+        openBrowser();
+        login();
+        logout();
+        driver.close();
+    }
+
+    public void openBrowser(){
+        driver = new SafariDriver();
+        driver.navigate().to("https://mail.google.com");
+        System.out.println("Safari browser Launched");
+    }
+
+    public void login(){
+        System.out.println("Safari - Login Successful");
+    }
+
+    public void logout(){
+        System.out.println("Safari - Logout Successful");
+    }
+
+}
+
+class ChromeTest implements Runnable {
+
+    WebDriver driver;
+
+    public void run(){
+        openBrowser();
+        login();
+        logout();
+        driver.quit();
+    }
+
+    public void openBrowser(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver=new ChromeDriver();
+        driver.navigate().to("https://mail.google.com");
+        System.out.println("Chrome browser Launched");
+    }
+
+    public void login(){
+        System.out.println("Chrome - Login Successful");
+    }
+
+    public void logout(){
+        System.out.println("Chrome - Logout Successful");
+    }
+
 
 }
